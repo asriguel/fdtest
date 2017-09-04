2af3febfa6985f22e3139a6ec9bbc02a6d3fcdda
a.petrakovich <petrakovich413@gmail.com>
2017-09-04 04:40:04 +0300

    first build

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Button extends Component {
+
+
+    render() {
+
+        return (
+            <button >
+                
+            </button>
+        );
+    }
+}
+
+
